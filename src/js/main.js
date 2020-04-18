f86587cf1f312aeb196ca1cef4ea61c3a5a73638
dim04ka <dmitry.suhotsky@gmail.com>
2020-04-18 13:55:56 +0300

    one

---
+++ src/js/main.js
@@ -0,0 +1,102 @@
+window.jQuery = require('jquery');
+var $ = require('jquery');
+var jQuery = require('jquery');
+
+ //auto complite adress
+
+
+(function($){
+
+
+    $(".form-send1").submit(function(e){
+        e.preventDefault();
+        var $form = $(this);
+
+        var car_in = document.getElementById('car_in');
+        console.log(car_in.value)
+    })
+    
+    // вкладки на главной
+    function formtabs() {
+        $('.tablinks').click(function(el){
+            var valTabs = $(this).val()
+            $('.tablinks').each(function(ind,el) {
+                $(el).removeClass('active')
+            })
+            $(this).addClass('active')
+
+            $('.main-dev').each(function(el){
+                $(this).removeClass('active');
+            })
+
+            $('.main-dev').each(function(el) {
+                
+                if ($(this).hasClass(valTabs)) {
+                    $(this).addClass('active');
+                }  
+            })
+        });
+    }
+    formtabs();
+
+    
+
+    //tarif-item-title slideShow/Hide
+    function tarifTabs(){
+        $('.tarif-item-title').click(function(){
+            $(this).children().toggleClass('active')
+            //$(this).next().toggleClass('active')
+            $(this).next().slideToggle('active')
+        })
+    }
+    tarifTabs();
+
+
+    //menu btn
+    $('.header-btn').click(function(){
+        $(this).toggleClass('active');
+        $('.header-ul').toggleClass('active');
+    })
+
+
+
+
+    // $('form').submit(function(e){
+    //     e.preventDefault();
+
+    //     sendAjaxForm('form', 'send2.php');
+      
+    //     function sendAjaxForm(ajax_form, url) {
+    //       $.ajax({
+    //           url:     url, //url страницы (send.php)
+    //           type:     "POST", //метод отправки
+    //           dataType: "html",
+    //           data: $(ajax_form).serialize(),  // Сеарилизуем объект
+    //           beforeSend: function(data) { // событие до отправки
+ 
+    //     		        },
+    //           success: function(response) { //Данные отправлены успешно
+    //             result = $.parseJSON(response);
+    //             console.log(result);
+    //             $("form").trigger("reset");
+    //             $('.content__input').css('display','none')
+    //             $('.reviews').css('display','block')
+
+    //             setTimeout( function(){
+    //                 $('.reviews').css('display','none')
+    //                 $('.content__input').css('display','block')
+    //                 $('.modal').css('display','none')
+    //             },1500)
+            
+    //         },
+    //         error: function(response) { // Данные не отправлены
+    //           console.log('Ошибка. Данные не отправлены.');
+    //         }
+    //      });
+    //     }
+
+
+    // })
+ 
+
+})(jQuery);
